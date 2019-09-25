6056c396e35b8693938243a737d073a442892810
co7744 <co7744@gmail.com>
2019-09-25 17:20:31 +0900

    Hi

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 console.log('main.js loaded')
 $(document).ready(function(){
     'use strict';
-    paper.install(window);
-    paper.setup(document.getElementById('mainCanvas'));
+    SVGAnimatedPreserveAspectRatio.install(window);
+    SVGAnimatedPreserveAspectRatio.setup(document.getElementById('mainCanvas'));
     var c;
     for(var x = 25; x < 400; x+=50){
         for(var y = 25; y < 400; y+= 50 ){
